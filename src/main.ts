37f3eccbebb8ade355e208fd92d160f6963ee6a5
chicken <hibary.taiwan@gmail.com>
2022-11-14 22:18:06 +0800

    chore: i18n

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
 import './style.css'
+import i18n from './lang/index'
 import router from './router/index'
 import App from './App.vue'
 
 
 const app = createApp(App)
 
-app.use(router).use(createPinia()).mount('#app')
+app.use(router)
+    .use(i18n)
+    .use(ElementPlus)
+    .use(createPinia())
+    .mount('#app')
